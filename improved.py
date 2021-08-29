5820fe7c5ef08fc7455770aeefa966e93fd5d760
ericlearning <48952432+ericlearning@users.noreply.github.com>
2021-08-30 04:25:21 +0900

    remove redundant calculation

--- improved.py
+++ improved.py
@@ -6,7 +6,8 @@ def improved_euler(f, y0, ts, args=()):
     for tn_prev, tn in zip(ts[:-1], ts[1:]):
         h = tn - tn_prev
         yn_prev = ys[-1]
-        yn_euler = yn_prev + h * f(yn_prev, tn_prev, *args)
-        yn = yn_prev + h * (f(yn_prev, tn_prev, *args) + f(yn_euler, tn, *args)) / 2
+        k = f(yn_prev, tn_prev, *args)
+        yn_euler = yn_prev + h * k
+        yn = yn_prev + h * (k + f(yn_euler, tn, *args)) / 2
         ys.append(yn)
     return np.stack(ys, 0)
